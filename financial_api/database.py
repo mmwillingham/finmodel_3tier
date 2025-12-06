c4f054ff6b0b420fd7fd986a43a6796baded0b6d
Martin Willingham <mwilling@mwilling-thinkpadp1gen4i.rmtusga.csb>
2025-12-06 16:35:05 -0500

    adjust

--- financial_api/database.py
+++ financial_api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace with your actual connection string (Host, Port, User, Password, DB Name)
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@host/dbname" 
+SQLALCHEMY_DATABASE_URL = "postgresql://bolauder:iamhe123@127.0.0.1/myapp" 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
