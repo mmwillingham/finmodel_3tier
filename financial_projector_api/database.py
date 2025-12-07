d3166e3c3ec082e3c6797e4b9100cd033aaa696b
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2025-12-07 18:03:32 -0500

    Update database.py

--- financial_projector_api/database.py
+++ financial_projector_api/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
 
 # Replace with your actual connection string (Host, Port, User, Password, DB Name)
 SQLALCHEMY_DATABASE_URL = "postgresql://bolauder:iamhe123@127.0.0.1/finmodel" 
