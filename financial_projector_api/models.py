5296f8344275af038b8c4e44b4b0170ec17432d4
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2025-12-08 14:25:57 -0500

    Update models.py

--- financial_projector_api/models.py
+++ financial_projector_api/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
 from datetime import datetime
