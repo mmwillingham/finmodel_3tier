d3546c3311f9acab882e3492adc8b452acbe58f0
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2025-12-08 11:32:43 -0500

    Update ProjectionDetail.js

--- financial_projector_ui/src/components/ProjectionDetail.js
+++ financial_projector_ui/src/components/ProjectionDetail.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { Line } from 'react-chartjs-2';
-import ProjectionService from ".../projection.service";
+import ProjectionService from "../projection.service";
 import { generateChartData, chartOptions } from '../utils/ChartConfig';
 import {
     Chart as ChartJS,
