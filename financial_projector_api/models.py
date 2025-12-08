72134b61ab4dafda06f70c620f84eaaca97131ef
Martin Willingham <37187589+mmwillingham@users.noreply.github.com>
2025-12-08 14:23:51 -0500

    Update models.py

--- financial_projector_api/models.py
+++ financial_projector_api/models.py
@@ -11,7 +11,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    
+    is_active = Column(Boolean, default=True)
     # Relationship to Projections: one user can have many projections
     projections = relationship("Projection", back_populates="owner")
 
