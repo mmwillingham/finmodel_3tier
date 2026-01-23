336ac9e6b5729a713e70b8aeb678c2e06d90f97b
Martin Willingham <martin.willingham@gmail.com>
2026-01-23 08:19:20 -0500

    fix alembic heads

--- api/alembic/versions/merge_ce90_null_brokerage.py
+++ api/alembic/versions/merge_ce90_null_brokerage.py
@@ -1,6 +1,6 @@
-"""merge_ce90_and_allow_null_brokerage
+"""merge_ce90_null_brokerage
 
-Revision ID: merge_ce90_and_allow_null_brokerage
+Revision ID: merge_ce90_null_brokerage
 Revises: ce90ec2a0c6f, allow_null_brokerage_on_accounts
 Create Date: 2026-01-23 12:00:00.000000
 
@@ -12,7 +12,7 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision: str = "merge_ce90_and_allow_null_brokerage"
+revision: str = "merge_ce90_null_brokerage"
 down_revision: Union[str, Sequence[str], None] = (
     "ce90ec2a0c6f",
     "allow_null_brokerage_on_accounts",
