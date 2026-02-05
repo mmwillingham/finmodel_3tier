a5d68f4ab9b69095f43cbaeae43b7e04d4585a2b
Martin Willingham <martin.willingham@gmail.com>
2026-02-05 15:20:32 -0500

    fix: network error

--- api/config.py
+++ api/config.py
@@ -50,7 +50,10 @@ class Settings(BaseSettings):
     MAIL_FROM: str | None = os.getenv("MAIL_FROM", "")
     MAIL_PORT: int = int(os.getenv("MAIL_PORT", 587))
     MAIL_SERVER: str | None = os.getenv("MAIL_SERVER", "")
-    CORS_ORIGINS_REGEX: str = os.getenv("CORS_ORIGINS_REGEX", r"^(http://localhost:3000|https://.*\.run\.app)$")
+    CORS_ORIGINS_REGEX: str = os.getenv(
+        "CORS_ORIGINS_REGEX",
+        r"^(http://localhost:3000|https://.*\.run\.app|https://ordaxium\.com|https://www\.ordaxium\.com|https://www\.modelmyretirement\.com)$",
+    )
     
     # Application name for emails
     APP_NAME: str = os.getenv("APP_NAME", "Financial Projector")
